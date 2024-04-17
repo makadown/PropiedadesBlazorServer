e8baff3c7e6cb0a833bbbd84271a3f763c29d78f
makadown <maseflores79@gmail.com>
2024-04-17 12:53:23 -0700

    JsRuntime helper (pendiente BORRAR propiedades)

---
+++ PropiedadesBlazor/Helpers/IJsHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace PropiedadesBlazor.Helpers
+{
+	public static class IJsHelper
+	{
+		public static async ValueTask ToastrSuccess(this IJSRuntime JSRuntime, string message)
+		{ 
+			await JSRuntime.InvokeVoidAsync("showToastr", "success", message);
+		}
+
+		public static async ValueTask ToastrError(this IJSRuntime JSRuntime, string message)
+		{ 
+			await JSRuntime.InvokeVoidAsync("showToastr", "error", message);
+		}
+
+		public static async ValueTask SwalSuccess(this IJSRuntime JSRuntime, string message)
+		{
+			await JSRuntime.InvokeVoidAsync("showSwal", "success", message);
+		}
+		public static async ValueTask SwalError(this IJSRuntime JSRuntime, string message)
+		{
+			await JSRuntime.InvokeVoidAsync("showSwal", "error", message);
+		}
+	}
+}
---
+++ PropiedadesBlazor/wwwroot/js/common.js
@@ -0,0 +1,20 @@
+﻿window.showToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Success", { timeOut: 10000 });
+    } else if (type === "info") {
+        toastr.info(message, "Info", { timeOut: 10000 });
+    } else if (type === "warning") {
+        toastr.warning(message, "Warning", { timeOut: 10000 });
+    } else if (type === "error") {
+        toastr.error(message, "Error", { timeOut: 10000 });
+    }
+}
+
+window.showSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire("Success", message, 'success');
+    }
+    if (type === "error") {
+        Swal.fire("Error", message, 'error');
+    }
+}
